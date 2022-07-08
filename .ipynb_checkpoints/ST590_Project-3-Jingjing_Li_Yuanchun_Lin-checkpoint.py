6f5778d22eafa8833186f202254f2eb058f73bc6
Jingjing Li <ji232@ncsu.edu>
2022-07-08 08:01:36 -0400

    code addition

--- .ipynb_checkpoints/ST590_Project-3-Jingjing_Li_Yuanchun_Lin-checkpoint.py
+++ .ipynb_checkpoints/ST590_Project-3-Jingjing_Li_Yuanchun_Lin-checkpoint.py
@@ -1,4 +1,6 @@
-# Set up for Creating Files
+##############################
+ST590_Project3-Jingjing_Li&YuanChun_Lin
+##############################
 
 ## Set up for Creating Files 
 
@@ -14,11 +16,12 @@ SA_df= acc_df[acc_df.pid=="SA0297"]
 PC_df= acc_df[acc_df.pid=="PC6771"]
 
 ### Write 500 values to the data
-for i in range (0, SA_df.shape[0]//500):
+#for i in range (0, SA_df.shape[0]//500):
+for i in range (0,10):
     SA_wrt=SA_df.loc[0:500]
     SA_wrt["timestamp"]=[time.strftime("%H:%M:%S",time.localtime())]*499
     SA_wrt.to_csv("prj3CSV/SA_wrt" + str(i) + ".csv",index=False,header = True)
-    PC_wrt=SA_df.loc[0:500]
+    mag_SA=math.sqrt(df_SA.x**2+df_SA.y**2+df_SA.z**2)_wrt=SA_df.loc[0:500]
     PC_wrt["timestamp"]=[time.strftime("%H:%M:%S",time.localtime())]*499
     PC_wrt.to_csv("prj3CSV2/PC_wrt" + str(i) + ".csv",index=False,header = True)
     time.sleep(20)
@@ -28,3 +31,17 @@ from pyspark.sql.types import StructType
 myschema=StructType().add("time","string").add("pid","string").add("x","integer").add("y","integer").add("z","integer").add("timestamp","timestamp")
 df_SA = spark.readStream.schema(myschema).csv("prj3CSV")
 df_PC = spark.readStream.schema(myschema).csv("prj3CSV2") 
+
+## Transform/Aggregation Step
+from math import sqrt
+from pyspark.sql.functions import *
+df_SA2=df_SA.withColumn("mag_SA",sqrt(col("x")**2+col("y")**2+col("z")**2))
+df_PC2=df_PC.withColumn("mag_SA",sqrt(col("x")**2+col("y")**2+col("z")**2))
+df_SA3=df_SA2.select(["time","pid","mag_SA"])
+df_PC3=df_PC2.select(["time","pid","mag_SA"])
+
+## Writing the Streams
+query_SA=df_SA3.writeStream.format("csv").outputMode("append").option("checkpointlocation","prj3CSV")
+query_PC=df_SA3.writeStream.format("csv").outputMode("append").option("checkpointlocation","prj3CSV2")
+query_SA.start()
+query_PC.start()
--- ST590_Project-3-Jingjing_Li_Yuanchun_Lin.py
+++ ST590_Project-3-Jingjing_Li_Yuanchun_Lin.py
@@ -17,11 +17,15 @@ PC_df= acc_df[acc_df.pid=="PC6771"]
 
 ### Write 500 values to the data
 #for i in range (0, SA_df.shape[0]//500):
-for i in range (0,10):
-    SA_wrt=SA_df.loc[0:500]
-    SA_wrt["timestamp"]=[time.strftime("%H:%M:%S",time.localtime())]*499
-    SA_wrt.to_csv("prj3CSV/SA_wrt" + str(i) + ".csv",index=False,header = True)
-    mag_SA=math.sqrt(df_SA.x**2+df_SA.y**2+df_SA.z**2)_wrt=SA_df.loc[0:500]
+for i in range (0,len(PC_df)//500):
+    if i < len(SA_df)//500:
+        SA_wrt=SA_df.loc[0:500]
+        SA_wrt["timestamp"]=[time.strftime("%H:%M:%S",time.localtime())]*499
+        SA_wrt.to_csv("prj3CSV/SA_wrt" + str(i) + ".csv",index=False,header = True)
+        
+    else:
+        continue
+    i +=1
     PC_wrt["timestamp"]=[time.strftime("%H:%M:%S",time.localtime())]*499
     PC_wrt.to_csv("prj3CSV2/PC_wrt" + str(i) + ".csv",index=False,header = True)
     time.sleep(20)
@@ -39,3 +43,29 @@ df_SA2=df_SA.withColumn("mag_SA",sqrt(col("x")**2+col("y")**2+col("z")**2))
 df_PC2=df_PC.withColumn("mag_SA",sqrt(col("x")**2+col("y")**2+col("z")**2))
 df_SA3=df_SA2.select(["time","pid","mag_SA"])
 df_PC3=df_PC2.select(["time","pid","mag_SA"])
+
+## Writing the Streams
+query_SA=df_SA3.writeStream.format("csv").outputMode("append").option("checkpointlocation","checkpoint_SA")
+query_SA.start()
+query_PC=df_SA3.writeStream.format("csv").outputMode("append").option("checkpointlocation","checkpoint_PC")
+query_PC.start()
+
+
+query_SA.stop()
+query_PC.stop()
+
+#Use PySpark to read in all "part" files
+all_SA= spark.read.option("header","false").csv("prj3CSV/SA_wrt*.csv")
+all_PC= spark.read.option("header","false").csv("prj3CSV2/PC_wrt*.csv")
+
+# Output as CSV file
+all_SA \
+.coalesce(1) \
+.write.format("csv") \
+.option("header", "false") \
+.save("prj3CSV/single_csv_SA/")
+all_PC \
+.coalesce(1) \
+.write.format("csv") \
+.option("header", "false") \
+.save("prj3CSV2/single_csv_PC/")
