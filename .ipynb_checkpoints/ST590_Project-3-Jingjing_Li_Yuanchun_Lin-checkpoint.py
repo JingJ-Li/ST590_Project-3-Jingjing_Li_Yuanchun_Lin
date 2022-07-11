1967e822b027c62ca32c55873ca79cc67b93e02b
Jingjing Li <ji232@ncsu.edu>
2022-07-11 18:19:10 -0400

    update

--- .ipynb_checkpoints/ST590_Project-3-Jingjing_Li_Yuanchun_Lin-checkpoint.py
+++ .ipynb_checkpoints/ST590_Project-3-Jingjing_Li_Yuanchun_Lin-checkpoint.py
@@ -6,6 +6,7 @@ ST590_Project3-Jingjing_Li&YuanChun_Lin
 
 ### Import modules
 import pandas as pd
+from pyspark.sql import SparkSession
 import time
 
 ### Read in data
@@ -17,31 +18,64 @@ PC_df= acc_df[acc_df.pid=="PC6771"]
 
 ### Write 500 values to the data
 #for i in range (0, SA_df.shape[0]//500):
-for i in range (0,10):
-    SA_wrt=SA_df.loc[0:500]
-    SA_wrt["timestamp"]=[time.strftime("%H:%M:%S",time.localtime())]*499
-    SA_wrt.to_csv("prj3CSV/SA_wrt" + str(i) + ".csv",index=False,header = True)
-    mag_SA=math.sqrt(df_SA.x**2+df_SA.y**2+df_SA.z**2)_wrt=SA_df.loc[0:500]
-    PC_wrt["timestamp"]=[time.strftime("%H:%M:%S",time.localtime())]*499
+for i in range (0,len(PC_df)//500):
+    if i < len(SA_df)//500:
+        SA_wrt=SA_df.iloc[0:500]
+        SA_wrt["timestamp"]=[time.strftime("%H:%M:%S",time.localtime())]*500
+        SA_wrt.to_csv("prj3CSV/SA_wrt" + str(i) + ".csv",index=False,header = True)
+        
+    else:
+        continue
+    i +=1
+    PC_wrt=PC_df.iloc[0:500]
+    PC_wrt["timestamp"]=[time.strftime("%H:%M:%S",time.localtime())]*500
     PC_wrt.to_csv("prj3CSV2/PC_wrt" + str(i) + ".csv",index=False,header = True)
     time.sleep(20)
     
 ## Reading a Stream    
 from pyspark.sql.types import StructType
-myschema=StructType().add("time","string").add("pid","string").add("x","integer").add("y","integer").add("z","integer").add("timestamp","timestamp")
-df_SA = spark.readStream.schema(myschema).csv("prj3CSV")
-df_PC = spark.readStream.schema(myschema).csv("prj3CSV2") 
+myschema=StructType().add("time","string").add("pid","string").add("x","float").add("y","float").add("z","float")
+df_SA = spark.readStream.schema(myschema).csv("prj3CSV/SA_wrt*.csv")
+df_PC = spark.readStream.schema(myschema).csv("prj3CSV2/PC_wrt*.csv") 
 
 ## Transform/Aggregation Step
 from math import sqrt
 from pyspark.sql.functions import *
 df_SA2=df_SA.withColumn("mag_SA",sqrt(col("x")**2+col("y")**2+col("z")**2))
-df_PC2=df_PC.withColumn("mag_SA",sqrt(col("x")**2+col("y")**2+col("z")**2))
+df_PC2=df_PC.withColumn("mag_PC",sqrt(col("x")**2+col("y")**2+col("z")**2))
 df_SA3=df_SA2.select(["time","pid","mag_SA"])
-df_PC3=df_PC2.select(["time","pid","mag_SA"])
+df_PC3=df_PC2.select(["time","pid","mag_PC"])
 
 ## Writing the Streams
-query_SA=df_SA3.writeStream.format("csv").outputMode("append").option("checkpointlocation","prj3CSV")
-query_PC=df_SA3.writeStream.format("csv").outputMode("append").option("checkpointlocation","prj3CSV2")
-query_SA.start()
-query_PC.start()
+query_SA=df_SA3.writeStream\
+               .format("csv")\
+               .option("checkpointlocation","checkpoint_SA")\
+               .option("path", "csv_SA")\
+               .outputMode("append")\
+               .start()
+query_PC=df_PC3.writeStream\
+               .format("csv")\
+               .option("checkpointlocation","checkpoint_PC")\
+               .option("path", "csv_PC")\
+               .outputMode("append")\
+               .start()
+
+
+query_SA.stop()
+query_PC.stop()
+
+#Use PySpark to read in all "part" files
+all_SA= spark.read.option("header","false").csv("csv_SA")
+all_PC= spark.read.option("header","false").csv("csv_PC")
+
+# Output as CSV file
+all_SA \
+.coalesce(1) \
+.write.format("csv") \
+.option("header", "false") \
+.save("prj3CSV/single_csv_SA/final_SA.csv")
+all_PC \
+.coalesce(1) \
+.write.format("csv") \
+.option("header", "false") \
+.save("prj3CSV2/single_csv_PC/final_PC.csv")
--- ST590_Project-3-Jingjing_Li_Yuanchun_Lin.py
+++ ST590_Project-3-Jingjing_Li_Yuanchun_Lin.py
@@ -6,6 +6,7 @@ ST590_Project3-Jingjing_Li&YuanChun_Lin
 
 ### Import modules
 import pandas as pd
+from pyspark.sql import SparkSession
 import time
 
 ### Read in data
@@ -19,62 +20,59 @@ PC_df= acc_df[acc_df.pid=="PC6771"]
 #for i in range (0, SA_df.shape[0]//500):
 for i in range (0,len(PC_df)//500):
     if i < len(SA_df)//500:
-        SA_wrt=SA_df.loc[0:500]
-        SA_wrt["timestamp"]=[time.strftime("%H:%M:%S",time.localtime())]*499
+        SA_wrt=SA_df.iloc[i*500:(i+1)500]
         SA_wrt.to_csv("prj3CSV/SA_wrt" + str(i) + ".csv",index=False,header = True)
-        
     else:
         continue
     i +=1
-    PC_wrt=PC_df.loc[0:500]
-    PC_wrt["timestamp"]=[time.strftime("%H:%M:%S",time.localtime())]*499
+    PC_wrt=PC_df.iloc[0:500]
     PC_wrt.to_csv("prj3CSV2/PC_wrt" + str(i) + ".csv",index=False,header = True)
     time.sleep(20)
     
 ## Reading a Stream    
 from pyspark.sql.types import StructType
-myschema=StructType().add("time","string").add("pid","string").add("x","integer").add("y","integer").add("z","integer").add("timestamp","timestamp")
-df_SA = spark.readStream.schema(myschema).csv("prj3CSV/SA_wrt*.csv")
-df_PC = spark.readStream.schema(myschema).csv("prj3CSV2/PC_wrt*.csv") 
+myschema=StructType().add("time","string").add("pid","string").add("x","float").add("y","float").add("z","float")
+df_SA = spark.readStream.schema(myschema).csv("prj3CSV")
+df_PC = spark.readStream.schema(myschema).csv("prj3CSV2") 
 
 ## Transform/Aggregation Step
 from math import sqrt
 from pyspark.sql.functions import *
 df_SA2=df_SA.withColumn("mag_SA",sqrt(col("x")**2+col("y")**2+col("z")**2))
-df_PC2=df_PC.withColumn("mag_SA",sqrt(col("x")**2+col("y")**2+col("z")**2))
+df_PC2=df_PC.withColumn("mag_PC",sqrt(col("x")**2+col("y")**2+col("z")**2))
 df_SA3=df_SA2.select(["time","pid","mag_SA"])
-df_PC3=df_PC2.select(["time","pid","mag_SA"])
+df_PC3=df_PC2.select(["time","pid","mag_PC"])
 
 ## Writing the Streams
 query_SA=df_SA3.writeStream\
                .format("csv")\
                .option("checkpointlocation","checkpoint_SA")\
                .option("path", "csv_SA")\
-               .outputMode("append")
-query_SA.start()
+               .outputMode("append")\
+               .start()
 query_PC=df_PC3.writeStream\
                .format("csv")\
                .option("checkpointlocation","checkpoint_PC")\
                .option("path", "csv_PC")\
-               .outputMode("append")
-query_PC.start()
+               .outputMode("append")\
+               .start()
 
 
 query_SA.stop()
 query_PC.stop()
 
 #Use PySpark to read in all "part" files
-all_SA= spark.read.option("header","false").csv("prj3CSV/SA_wrt*.csv")
-all_PC= spark.read.option("header","false").csv("prj3CSV2/PC_wrt*.csv")
+all_SA= spark.read.option("header","false").csv("csv_SA")
+all_PC= spark.read.option("header","false").csv("csv_PC")
 
 # Output as CSV file
 all_SA \
 .coalesce(1) \
 .write.format("csv") \
 .option("header", "false") \
-.save("prj3CSV/single_csv_SA/")
+.save("prj3CSV/single_csv_SA/final_SA.csv")
 all_PC \
 .coalesce(1) \
 .write.format("csv") \
 .option("header", "false") \
-.save("prj3CSV2/single_csv_PC/")
+.save("prj3CSV2/single_csv_PC/final_PC.csv")
