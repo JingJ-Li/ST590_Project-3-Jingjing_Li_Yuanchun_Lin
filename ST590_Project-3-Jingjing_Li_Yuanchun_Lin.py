865d6c311c3f6eae65bc13784d8c4e78c25309ad
Jingjing Li <ji232@ncsu.edu>
2022-07-12 20:54:11 -0400

    set up a code based peter's code and modify it

--- ST590_Project-3-Jingjing_Li_Yuanchun_Lin.py
+++ ST590_Project-3-Jingjing_Li_Yuanchun_Lin.py
@@ -7,6 +7,9 @@ ST590_Project3-Jingjing_Li&YuanChun_Lin
 ### Import modules
 import pandas as pd
 import time
+from math import sqrt
+from pyspark.sql.functions import *
+from pyspark.sql.types import StructType
 
 ### Read in data
 acc_df = pd.read_csv ("data/all_accelerometer_data_pids_13.csv")
@@ -15,8 +18,7 @@ acc_df = pd.read_csv ("data/all_accelerometer_data_pids_13.csv")
 SA_df= acc_df[acc_df.pid=="SA0297"]
 PC_df= acc_df[acc_df.pid=="PC6771"]
 
-### Write 500 values to the data
-#for i in range (0, SA_df.shape[0]//500):
+### Write 500 values at a time to a csv file
 for i in range (0,len(PC_df)//500):
     if i < len(SA_df)//500:
         SA_wrt=SA_df.iloc[i*500:(i+1)*500]
@@ -28,16 +30,16 @@ for i in range (0,len(PC_df)//500):
     time.sleep(20)
     
 ## Reading a Stream    
-from pyspark.sql.types import StructType
 myschema=StructType().add("time","string").add("pid","string").add("x","float").add("y","float").add("z","float")
 df_SA = spark.readStream.schema(myschema).csv("prj3CSV")
 df_PC = spark.readStream.schema(myschema).csv("prj3CSV2") 
 
 ## Transform/Aggregation Step
-from math import sqrt
-from pyspark.sql.functions import *
+### Add new columns with mega value
 df_SA2=df_SA.withColumn("mag_SA",sqrt(col("x")**2+col("y")**2+col("z")**2))
 df_PC2=df_PC.withColumn("mag_PC",sqrt(col("x")**2+col("y")**2+col("z")**2))
+
+### Select desirable columns 
 df_SA3=df_SA2.select(["time","pid","mag_SA"])
 df_PC3=df_PC2.select(["time","pid","mag_PC"])
 
@@ -55,20 +57,23 @@ query_PC=df_PC3.writeStream\
                .outputMode("append")\
                .start()
 
-
+### Stop stream data query
 query_SA.stop()
 query_PC.stop()
 
-#Use PySpark to read in all "part" files
+## Use PySpark to read in all "part" files
 all_SA= spark.read.option("header","false").csv("csv_SA")
 all_PC= spark.read.option("header","false").csv("csv_PC")
 
-# Output as CSV file
+## Output as CSV file
+### Output SA0297 file
 all_SA \
 .coalesce(1) \
 .write.format("csv") \
 .option("header", "false") \
 .save("prj3CSV/single_csv_SA")
+
+### Output PC6771 file
 all_PC \
 .coalesce(1) \
 .write.format("csv") \
